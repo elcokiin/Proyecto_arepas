7ab66c403f845c0e7d1ccda92b579f8d7a93f3c2
Julian Archila <julialejo2018@gmail.com>
2021-04-12 12:33:03 -0500

    Cambio al render de los resultados.

--- main.js
+++ main.js
@@ -6,36 +6,40 @@ const boton = document.querySelector('#boton');
 boton.addEventListener('click', enviar = () => {
     resPaquetes = parseFloat(paquetes.value);
     resTipo = tipo.value;
-    if (resTipo == 'sal' || resTipo == 'azucar' || resTipo == 'Sal'|| resTipo == 'Azucar' || resTipo == 1 || resTipo == 2) {
+    if (resTipo == 'sal' || resTipo == 'azucar' || resTipo == 'Sal' || resTipo == 'Azucar' || resTipo == 1 || resTipo == 2) {
         if (resPaquetes > 0) {
-            todo('maiz',35,60);
-            todo('mantequilla',2.5,60);
+            todo('maiz', 35, 60);
+            todo('mantequilla', 2.5, 60);
             if (resTipo == 'sal' || resTipo == 'Sal' || resTipo == 1) {
-                todo('sal',0.5,35);
+                todo('sal', 0.5, 35);
             }
-            else{
-                todo('azucar',2,25);
+            else {
+                todo('azucar', 2, 25);
             }
         }
-        else{
+        else {
             alert('Inserte un número de paquetes valido');
         }
     }
-    else{
+    else {
         alert('El tipo de arepa no se a encontrado');
     }
 })
 
-const todo = (matter,amount,num) => {
+const todo = (matter, amount, num) => {
     resPaquetes = parseFloat(paquetes.value);
-    operacion = amount*resPaquetes/num;
+    operacion = amount * resPaquetes / num;
+    const resultsObj = document.getElementById("results");
     // operacion = Math.round(operacion);
     operacion = operacion.toFixed(2);
     if (operacion < 1) {
         operacion *= 500;
-        document.write(`La cantidad de <b>${matter}</b> que se necesita para <b>${resPaquetes}</b> paquetes es <b>${operacion} gramos</b><br>`);    
+        resultsObj.innerHTML = `<p>La cantidad de <b>${matter}</b> que se necesita para <b>${resPaquetes}</b> paquetes es <b>${operacion} gramos</b><br></p>`
+
+        // document.write(`La cantidad de <b>${matter}</b> que se necesita para <b>${resPaquetes}</b> paquetes es <b>${operacion} gramos</b><br>`);    
     }
-    else{
-        document.write(`La cantidad de <b>${matter}</b> que se necesita para <b>${resPaquetes}</b> paquetes es <b>${operacion} libras</b><br>`);
+    else {
+        resultsObj.innerHTML = `<p>La cantidad de <b>${matter}</b> que se necesita para <b>${resPaquetes}</b> paquetes es <b>${operacion} libras</b><br></p>`
+        // document.write(`La cantidad de <b>${matter}</b> que se necesita para <b>${resPaquetes}</b> paquetes es <b>${operacion} libras</b><br>`);
     }
 }
